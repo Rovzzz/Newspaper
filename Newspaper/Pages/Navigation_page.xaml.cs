e54f218d48d91139d66ea826394d89c23610fede
zlobinovr@gmail.com <zlobinovr@gmail.com>
2023-06-01 15:05:18 +0300

    Добавьте файлы проекта.

---
+++ Newspaper/Pages/Navigation_page.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Newspaper.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Newspaper.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Navigation_page.xaml
+    /// </summary>
+    public partial class Navigation_page : Page
+    {
+        public Navigation_page()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNavigateAgents_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Pages.Page_Agents());
+        }
+
+        private void btnNavigateSale_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Pages.Page_Sale());
+        }
+
+        private void btnNavigateProducts_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Pages.Page_Products());
+        }
+    }
+}
